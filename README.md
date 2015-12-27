c20b4230c44c4ad8254fb72c0c7d28b34e413d85
j2le <jeffreytooley@gmail.com>
2015-12-27 15:19:08 -0800

    clean up of comments.

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
 # spacey
 2D space sim game
  * Uses pygame
+
+# top priority
+#  *get on orbit looking solid
+#  *parallax
+#  *infinite zoom for vector drawings
+#  *icons
+#  *start on a board
+#  *sidebar options
+#  *provide zoom based icons
+#  *define atmosphere parameters for each planet object thing
+#  *differentiate between inertial and earth relative velocity 
+#  *make moon horzon work like earth horizon view for propulsive landing 
+#  *optionally scale the rocket with zoom
+#  *j2 effects for asteroids
+#  *rendezvous problem
+#  *add a zoom that includes landing point
+#  *make zooms slick rather than cheap
+#  * make boards
+
+# fix time zoom to have more options
+#  * Pause
+# add boards
+# Zoom and mult-screen
+#   * Click zoom to
+#   * split screen into 2 screens 
+# make display not jittery
+# add lift and drag vector
+# reentry attitude dynamics
+# move reentry flap
+# stage separation model
+# cubic earth gravity field
+# make interpolation smooth
+# compute the angle of attack
+# make vehicle move relative to altitude in horizon view
+# change parent body for reference
+# switch when reaching sphere of influence of new body
+# add propellant for attitude control
+# add 3-view 
+# add sun and interplanetary
+# make read out better
+# draw a line to aries and aquarius
+# Draw Earth land shadows
+# slow the rate of change of the orbit
+# Draw vector to the sun
+# Draw vector to the moon
+# add arrows to various planets and features
+# add a ground station
+# add the sun
+# add display for time sun
+# switch to trim thrusters
+# @todo display sim time versus real time
+# make a board, one that teaches something
+#  * reach a target orbit
+#  * stay in communication with a sat
+# Amazing space pictures
+#  * Earth and Moon from magellan
+#  * Saturn from Gailleo leaving
+# add heating indicators
+# add flight path angles
+# add separation feature
+# allow controlling of multiple stages
+# add parachutes
+# propagation of 3rd body needs to be added to predictor
+
+
+# Fancify the following
+# altitude marker
+# make navigation optional
+
--- src/spacey.py
+++ src/spacey.py
@@ -27,75 +27,6 @@ import gradients
 from gradients import genericFxyGradient
 import random
 
-# top priority
-#  *get on orbit looking solid
-#  *parallax
-#  *infinite zoom for vector drawings
-#  *icons
-#  *start on a board
-#  *sidebar options
-#  *provide zoom based icons
-#  *define atmosphere parameters for each planet object thing
-#  *differentiate between inertial and earth relative velocity 
-#  *make moon horzon work like earth horizon view for propulsive landing 
-#  *optionally scale the rocket with zoom
-#  *j2 effects for asteroids
-#  *rendezvous problem
-#  *add a zoom that includes landing point
-#  *make zooms slick rather than cheap
-#  * make boards
-
-# fix time zoom to have more options
-#  * Pause
-# add boards
-# Zoom and mult-screen
-#   * Click zoom to
-#   * split screen into 2 screens 
-# make display not jittery
-# add lift and drag vector
-# reentry attitude dynamics
-# move reentry flap
-# stage separation model
-# cubic earth gravity field
-# make interpolation smooth
-# compute the angle of attack
-# make vehicle move relative to altitude in horizon view
-# change parent body for reference
-# switch when reaching sphere of influence of new body
-# add propellant for attitude control
-# add 3-view 
-# add sun and interplanetary
-# make read out better
-# draw a line to aries and aquarius
-# Draw Earth land shadows
-# slow the rate of change of the orbit
-# Draw vector to the sun
-# Draw vector to the moon
-# add arrows to various planets and features
-# add a ground station
-# add the sun
-# add display for time sun
-# switch to trim thrusters
-# @todo display sim time versus real time
-# make a board, one that teaches something
-#  * reach a target orbit
-#  * stay in communication with a sat
-# Amazing space pictures
-#  * Earth and Moon from magellan
-#  * Saturn from Gailleo leaving
-# add heating indicators
-# add flight path angles
-# add separation feature
-# allow controlling of multiple stages
-# add parachutes
-# propagation of 3rd body needs to be added to predictor
-
-
-# Fancify the following
-# altitude marker
-# make navigation optional
-
-# =======================================
 # =======================================
 
 # Initialize the game engine
@@ -112,7 +43,7 @@ screen=pygame.display.set_mode(game_constants.size)
 #Loop until the user clicks the close button.
 game = game(pygame)
 
-# initialize moon
+# initialize earth
 earth = earth()
 
 # initialize moon
@@ -130,7 +61,7 @@ sim = sim()
 # initialize flight computer
 flight_computer = flight_computer()
 
-# begin the loop fro the game
+# begin the loop for the game
 while game.done==False:
 
     # http://www.pygame.org/docs/ref/key.html
